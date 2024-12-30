96a7b3a892770fe3cb1963438f93926889cf8d7b
NilsGke <n.j.goeke@gmail.com>
2024-12-30 02:15:22 +0100

    heading

--- frontend/src/pages/indexPage.tsx
+++ frontend/src/pages/indexPage.tsx
@@ -3,15 +3,21 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function RootPage() {
   return (
-    <div className="flex items-center justify-center size-full">
-      <Card>
-        <CardHeader>
-          <CardTitle>Your Images</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Images />
-        </CardContent>
-      </Card>
+    <div className="flex flex-col max-h-[85vh] h-[85vh]">
+      <div className="flex items-center justify-center h-full max-h-60 min-h-40">
+        <h1 className="font-serif text-6xl text-center">Paper Display</h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center overflow-hidden">
+        <Card className="flex flex-col max-h-screen overflow-hidden max-w-[90%]">
+          <CardHeader>
+            <CardTitle>Your Images</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col overflow-auto">
+            <Images />
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
